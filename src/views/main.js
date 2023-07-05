0302fd72ff59216a11bef50c52bf5fe03e151fa7
nol4lej <nome.ahumada@outlook.com>
2023-07-05 03:47:16 -0400

    Main render by category

--- src/views/main.js
+++ src/views/main.js
@@ -4,24 +4,53 @@ import { ClienteServices } from "../controllers/crud-controller.js"
 function FetchProducts(){
     ClienteServices.GetProducts()
     .then((data) => {
-        data.reverse().forEach((element, index) => {
-            if(index < 6){
-                const { name, imageUrl, price, description, id} = element
-                AddProduct(name, imageUrl, price)
-            } 
+        const contadores = {
+            starwars: 0,
+            consola: 0,
+            otros: 0
+        }
+        data.reverse().forEach(element => {
+            const verifyCategory = element.category;
+            switch (verifyCategory) {
+                case "starwars":
+                    if(contadores.starwars < 6){
+                        contadores.starwars++
+                        const { name, imageUrl, price, description, id, category} = element
+                        RenderItems(name, imageUrl, price, category)
+                    }
+                    break;
+                case "consola":
+                    if(contadores.consola < 6){
+                        contadores.consola++
+                        const { name, imageUrl, price, description, id, category} = element
+                        RenderItems(name, imageUrl, price, category)
+                        
+                    }
+                    break;
+                case "otros":
+                    if(contadores.otros < 6){
+                        contadores.otros++
+                        const { name, imageUrl, price, description, id, category} = element
+                        RenderItems(name, imageUrl, price, category)
+                    }
+                    break;
+                default:
+                    break;
+            }
         });
     })
     .catch((error) => console.error(error));
 }
 
-function AddProduct(name, imageUrl, price){
-    const father = document.getElementById("items")
+function RenderItems(name, imageUrl, price, category){
+    const father = document.getElementById(`${category}`)
     const div = document.createElement("div")
     div.classList.add("item")
     div.innerHTML = `
         <img class="item__img" src="${imageUrl}" alt="">
         <h3 class="item__title">${name}</h3>
         <p class="item__price">${price}</p>
+        <p class="item__price">${category}</p>
         <a class="item__link" href="#">Ver Producto</a>
     `
     father.appendChild(div)
@@ -43,7 +72,23 @@ export function Main(){
                 <h2 class="products__title">Star Wars</h2>
                 <a href="" class="products__link">Ver todo <i class="material-icons">arrow_forward</i></a>
             </div>
-            <div class="items" id="items"></div>
+            <div class="items" id="starwars"></div>
+        </section>
+
+        <section class="products">
+            <div class="products__header">
+                <h2 class="products__title">Consolas</h2>
+                <a href="" class="products__link">Ver todo <i class="material-icons">arrow_forward</i></a>
+            </div>
+            <div class="items" id="consola"></div>
+        </section>
+
+        <section class="products">
+            <div class="products__header">
+                <h2 class="products__title">Otros</h2>
+                <a href="" class="products__link">Ver todo <i class="material-icons">arrow_forward</i></a>
+            </div>
+            <div class="items" id="otros"></div>
         </section>
 
     `
