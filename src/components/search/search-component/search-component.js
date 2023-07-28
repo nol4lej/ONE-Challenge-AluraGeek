d4614fd28e2c4136da42cf61d8f6695ff54ba03b
nol4lej <nome.ahumada@outlook.com>
2023-07-28 01:06:51 -0400

    Search input target captured and products filtered per starsWith and includes

--- src/components/search/search-component/search-component.js
+++ src/components/search/search-component/search-component.js
@@ -1,3 +1,5 @@
+import { state } from "../../../state/state.js"
+
 export class SearchComponent extends HTMLElement{
 
     constructor(){
@@ -6,15 +8,40 @@ export class SearchComponent extends HTMLElement{
 
     connectedCallback(){
         this.render()
+        this.handleSearchInput()
     }
 
     render(){
         this.innerHTML = `
             <div class="nav__search">
-                <input class="search__input" type="text" placeholder="¿Qué deseas buscar?">
+                <input id="search-input" class="search__input" type="text" placeholder="¿Qué deseas buscar?">
                 <button class="material-icons search__button">search</button>
             </div>
         `
+    }
+
+    handleSearchInput(){
+        const input = this.querySelector("#search-input")
+
+        input.addEventListener("input", (event) => {
+            const inputValue = event.target.value.toLowerCase()
+
+            // condicion que limpia cuando no haya input
+            if(inputValue === ""){
+                input.value = ""
+                return
+            }
+
+            // caputo los productos que su "name" comience con las letras que yo ingrese en el input.
+            const results = state.products.filter(product => {
+                return product.name.toLowerCase().startsWith(inputValue) || product.name.toLowerCase().includes(inputValue)
+            })
+            console.log(results)
+
+
+        })
+
+
     }
 
 }
