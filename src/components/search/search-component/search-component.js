0c32850b322adaed0a061d7d7a5f132a7330205a
nol4lej <nome.ahumada@outlook.com>
2023-07-28 03:53:41 -0400

    Stable version - Handles added for search component (url, button, responsive)

--- src/components/search/search-component/search-component.js
+++ src/components/search/search-component/search-component.js
@@ -4,12 +4,14 @@ export class SearchComponent extends HTMLElement{
 
     constructor(){
         super()
+        this.searchUrl = ""; // aqui se almacena la url que se crea para la busqueda
     }
 
     connectedCallback(){
         this.render()
         this.handleSearchInput()
         this.handleSearchWidth()
+        this.handleClickButton()
     }
 
     render(){
@@ -22,32 +24,77 @@ export class SearchComponent extends HTMLElement{
     }
 
     handleSearchInput(){
-        const input = this.querySelector("#search-input")
+        const searchInput = this.querySelector("#search-input")
+        const btnSearch = this.querySelector("#search-button")
 
-        input.addEventListener("input", (event) => {
+        searchInput.addEventListener("input", (event) => {
             const inputValue = event.target.value.toLowerCase()
 
-            // condicion que limpia cuando no haya input
+            // Limpio la url de los productos acumulados anteriormente.
+            btnSearch.setAttribute("href", `#`)
+            this.searchUrl = ""
+
+            // 1.- Condicion que limpia la url
             if(inputValue === ""){
-                input.value = ""
+                this.searchUrl = ""
+                btnSearch.setAttribute("href", `#`)
+                // searchInput.value = ""
                 return
             }
 
-            // caputo los productos que su "name" comience con las letras que yo ingrese en el input.
+            // 2.- caputo los productos que su "name" comience con las letras que yo ingrese en el input.
             const results = state.products.filter(product => {
                 return product.name.toLowerCase().startsWith(inputValue) || product.name.toLowerCase().includes(inputValue)
             })
+            
             console.log(results)
-
+            // 3.- preparo los productos para renderizar
+            this.prepareProducts(results)
 
         })
+    }
 
+    prepareProducts(results){
+        const resultsFilter = results.map(product => {
+            const item = document.createElement("li")
+            item.innerHTML = `
+                <a href="#/id=${product.id}" class="search__item">
+                    <img class="search__img" src="${product.imageUrl}" alt="${product.name}">
+                    <p>${product.name.slice(0,20)}</p>
+                </a>
+            `
+            // capturo el id y creo la url para el boton search
+            this.createUrl(product.id)
+            return item
+        })
+    }
 
+    createUrl(id){
+        const btnSearch = this.querySelector("#search-button")
+        if(this.searchUrl === ""){
+            this.searchUrl = id.slice(0,8)
+        } else {
+            this.searchUrl = this.searchUrl+"+"+id.slice(0,8)
+        }
+        btnSearch.setAttribute("href", `#/?search=${this.searchUrl}`)
+    }
+
+    // metodo que controla que al hacer click en el boton search, se limpie el input y la url
+    handleClickButton(){
+        const btnSearch = this.querySelector("#search-button")
+        const searchInput = this.querySelector("#search-input")
+        btnSearch.addEventListener("click", () => {
+            setTimeout(() => {
+                this.searchUrl = ""
+                btnSearch.setAttribute("href", `#`)
+            }, 100);
+            searchInput.value = ""
+        })
     }
 
     // metodo que controla la activacion search en responsive
     handleSearchWidth(){
-        const btn = this.querySelector("#search-button")
+        const btnSearch = this.querySelector("#search-button")
         const searchContainer = this.querySelector("#nav-search-container")
         const searchInput = this.querySelector("#search-input")
         
@@ -60,12 +107,12 @@ export class SearchComponent extends HTMLElement{
     
         const activateListener = () => {
             if (!isListenerActive && innerWidth < 768) {
-                btn.addEventListener("click", toggleSearch);
+                btnSearch.addEventListener("click", toggleSearch);
                 isListenerActive = true;
             } else if (isListenerActive && innerWidth >= 768) {
                 searchContainer.classList.remove("active");
                 searchInput.classList.remove("active");
-                btn.removeEventListener("click", toggleSearch);
+                btnSearch.removeEventListener("click", toggleSearch);
                 isListenerActive = false;
             }
         };
